b89cd1047ab7c2e1ed3b2d792220221dc2813446
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-04-25 15:39:48 +0200

    clean code

--- src/main/java/pl/szczesniak/dominik/App.java
+++ src/main/java/pl/szczesniak/dominik/App.java
@@ -3,9 +3,11 @@ package pl.szczesniak.dominik;
 import pl.szczesniak.dominik.pointsale.PointOfSaleConsoleApp;
 
 public class App {
+
     public static void main( String[] args ) {
 
         new PointOfSaleConsoleApp().run();
 
     }
+    
 }
