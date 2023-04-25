04c46de763d12ac27bcba18c3156e338c3ccd5a1
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-04-25 14:44:08 +0200

    deleted exceptions

--- src/main/java/pl/szczesniak/dominik/pointsale/devices/barcodescanner/domain/model/exceptions/InvalidBarcodeException.java
+++
@@ -1,8 +0,0 @@
-package pl.szczesniak.dominik.pointsale.devices.barcodescanner.domain.model.exceptions;
-
-public class InvalidBarcodeException extends RuntimeException {
-
-	public InvalidBarcodeException(final String message) {
-		super(message);
-	}
-}
--- src/main/java/pl/szczesniak/dominik/pointsale/devices/barcodescanner/domain/model/exceptions/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pl.szczesniak.dominik.pointsale.devices.barcodescanner.domain.model.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-
-	public ProductNotFoundException(final String message) {
-		super(message);
-	}
-}
